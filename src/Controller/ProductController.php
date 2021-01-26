6fb429d01bdbd9cf694f41d50e30552a4f62dd71
vagrant <vagrant@homestead>
2021-01-26 10:53:13 +0000

    Product filter list method

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -3,6 +3,8 @@
 namespace App\Controller;
 
 use App\Entity\Product;
+use App\Entity\ProductFilter;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -21,21 +23,60 @@ class ProductController extends AbstractController
 
     public function createProduct(): Response
     {
-        // you can fetch the EntityManager via $this->getDoctrine()
-        // or you can add an argument to the action: createProduct(EntityManagerInterface $entityManager)
         $entityManager = $this->getDoctrine()->getManager();
 
         $product = new Product();
         $product->setName('Keyboard');
         $product->setPrice(1999);
         $product->setDescription('Ergonomic and stylish!');
+        $product->setCode('Code');
+        $product->setCatalogId(1);
 
-        // tell Doctrine you want to (eventually) save the Product (no queries yet)
         $entityManager->persist($product);
 
-        // actually executes the queries (i.e. the INSERT query)
         $entityManager->flush();
 
         return new Response('Saved new product with id '.$product->getId());
     }
+
+    public function showAction()
+    {
+        $product = $this->getDoctrine()
+            ->getRepository('App:Product')
+            ->findAll();
+        $data = [
+            'product'=> $product
+        ];
+
+
+        return new Response('Show products'.$data);
+
+    }
+
+    public function filtersProductList(Request $request): string
+    {
+//        написать сервис который по id продукта возвращает массив с фильтрами (ProductFilters) для этого продукта
+        $id = $request->request->get("id");
+        
+        $product = $this->getDoctrine()->getRepository('App:Product')->findOneById($id);
+        $productFilter = $request->request->get("filterGroupCode");
+
+        $productFilter = new ProductFilter(
+
+        );
+
+        $product->findOneById($product);
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->persist($productFilter);
+        $entityManager->flush();
+
+        $data = [
+            'filterGroupCode' => $productFilter,
+        ];
+
+        return new Response('Hello '. $product->getfilterGroupCode("id"));
+
+    }
+
+
 }
